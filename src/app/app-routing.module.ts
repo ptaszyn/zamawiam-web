823143d9ed0799e68214c56be40c54aa267a4a73
unknown <damian.ptaszynski@gmail.com>
2019-02-21 22:58:26 +0100

    RELEASE 0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
-import { OrderPackNewComponent } from './orders/order-pack-new/order-pack-new.component';
-import { OrderPackDetailComponent } from './orders/order-pack-detail/order-pack-detail.component';
-import { OrderHeadDetailComponent } from './orders/order-head-detail/order-head-detail.component';
 
 const routes: Routes = [
   {
@@ -16,16 +13,8 @@ const routes: Routes = [
     loadChildren: './restaurants/restaurants.module#RestaurantsModule'
   },
   {
-    path: 'orderpacks/new',
-    component: OrderPackNewComponent
-  },
-  {
-    path: 'orderpacks/:id',
-    component: OrderPackDetailComponent
-  },
-  {
-    path: 'orderpacks/:idPack/order/:id',
-    component: OrderHeadDetailComponent
+    path: 'orderpacks',
+    loadChildren: './orders/orders.module#OrdersModule'
   },
   {
     path: '',
